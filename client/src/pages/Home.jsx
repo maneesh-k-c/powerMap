3d3fba78b46cbbeac6e5d7a06f60d6d2bf3173e8
maneeshk-chandran <maneesh.maitexa@gmail.com>
2025-02-19 22:25:25 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -104,7 +104,7 @@ export default function Home() {
                                 <div className="row justify-content-center py-5 mb-5">
                                     <div className="col-lg-10 pt-lg-5 mt-lg-5 text-center " >
                                         <h1 className="display-3 text-white mb-3 animated slideInDown">
-                                            Seamless Charging,<br /> Wherever You Go.
+                                            Earn With Every<br /> Charge
                                         </h1>
                                         <p className="fs-4 text-white mb-4 animated slideInDown">
                                             Find - Book - Charge
